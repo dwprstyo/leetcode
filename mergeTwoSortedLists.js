b4d06df3f377eb2f2633d3c93acdd5a24202e7c5
juangsyah.putra@bni.co.id <kuatdwiprasetiyo@gmail.com>
2023-07-18 19:31:15 +0700

    merge two sorted list

---
+++ mergeTwoSortedLists.js
@@ -0,0 +1,23 @@
+function mergeTwoLists(list1, list2) {
+    let compare = 0;
+    let result = [];
+
+    if (list1.length >= list2.length) {
+        compare = list1.length
+    }else{
+        compare = list2.length
+    }
+
+    for (let i = 0; i < compare; i++) {
+        if (list1[i] != undefined) {
+            result.push(list1[i])
+        }
+        if (list2[i] != undefined) {
+            result.push(list2[i])
+        }
+    }
+
+    return result.sort();
+};
+
+console.log(mergeTwoLists([2,4,5],[1,2,3]));
