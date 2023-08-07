e215455c51628899dc4a13c010285fc4a89c9ef6
juangsyah.putra@bni.co.id <kuatdwiprasetiyo@gmail.com>
2023-08-08 00:34:20 +0700

    climbing stairs diluar nalar wkwk

--- climbingStairs.js
+++ climbingStairs.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    
+    if (n < 3) return n;
+
+    let a = 1;
+    let b = 2;
+
+    for (let i = 2; i < n; i ++) {
+        let c = b;
+        b = b + a;
+        a = c;
+    }
+
+    return b;
 };
+
+console.log(climbStairs(3));
